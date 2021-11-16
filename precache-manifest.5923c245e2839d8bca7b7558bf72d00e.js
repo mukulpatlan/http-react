5fc45d313e4f1202db8aa8b29f3200caa6b6701a
Mukul Patlan <mukulviket@gmail.com>
2021-11-16 18:13:52 +0530

    Updates

---
+++ precache-manifest.5923c245e2839d8bca7b7558bf72d00e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea812643aa8100f9975ca4600c60002b",
+    "url": "/http-react/index.html"
+  },
+  {
+    "revision": "921cadc6fa10ce352715",
+    "url": "/http-react/static/css/main.102d0df9.chunk.css"
+  },
+  {
+    "revision": "6c839178af36f6ed2278",
+    "url": "/http-react/static/js/2.ee07fe34.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/http-react/static/js/2.ee07fe34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921cadc6fa10ce352715",
+    "url": "/http-react/static/js/main.ffced3f2.chunk.js"
+  },
+  {
+    "revision": "d6e275c88e02db974be7",
+    "url": "/http-react/static/js/runtime-main.01cea0ef.js"
+  }
+]);
